7779f0266293656e6fc634e2c1d0833f2648dc2e
bryanhmiller <bryankalos@hotmail.com>
2017-02-18 13:37:26 -0600

    Added placeholder for input, and name for button in index.html, also adjusted button size and text in styles.css, and removed console.logs from strings.js

--- strings.js
+++ strings.js
@@ -1,19 +1,19 @@
 // 1. Copy the code below into the JavaScript file. (Completed)
 // 3. The text input should only accept letters. No numbers. (set input type as "text")
 var testString = "";
+var reversedResult = "";
+
+
 
 function myUserInput() {
 	testString = document.getElementById("userInput").value;
-	console.log(testString);
 }
-
 
 // 4. Implement the logic in the reversal function that reverses the order of the characters in the 
 // string, and outputs the result in the DOM, below the text input.
 
 function reversal() {
-	var reversedResult = testString.split("").reverse().join("");
-	console.log(reversedResult);
+	reversedResult = testString.split("").reverse().join("");
 	document.getElementById("results").innerHTML = "<h1>" + reversedResult + "</h1>";
 }
 
@@ -21,14 +21,15 @@ function reversal() {
 // and outputs the result in the DOM, below the text input.
 function alphabits() {
 	var alphabetizedResult = testString.split("").sort().join("");
-	console.log(alphabetizedResult);
 	document.getElementById("results").innerHTML += "<h1>" + alphabetizedResult + "</h1>";
 }
 
 // 6. Implement the logic in the palindrome function that determine whether the string is a palindrome. 
 // If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
 function palindrome() {
-
+	if (testString === reversedResult) {
+		document.getElementById("results").innerHTML += "<h1>Your string is a palindrome!</h1>";		
+	}
 }
 
 
