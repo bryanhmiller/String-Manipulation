43e37e77977aaa73cc880e88da24e283e857e52c
bryanhmiller <bryankalos@hotmail.com>
2017-02-18 11:58:59 -0600

    Created index.html and linked strings.js, entered basic html requirements for exercise, and copied instructions and commented them out and distributed them in strings.js

---
+++ strings.js
@@ -0,0 +1,29 @@
+// 1. Copy the code below into the JavaScript file. (Completed)
+// 3. The text input should only accept letters. No numbers. (set input type as "text")
+
+
+// 4. Implement the logic in the reversal function that reverses the order of the characters in the 
+// string, and outputs the result in the DOM, below the text input.
+function reversal() {
+
+}
+
+// 5. Implement the logic in the alphabits function that return the characters in alphabetical order, 
+// and outputs the result in the DOM, below the text input.
+function alphabits() {
+
+}
+
+// 6. Implement the logic in the palindrome function that determine whether the string is a palindrome. 
+// If it is, display the text "Your string is a palidrome" in the DOM, below the text input.
+function palindrome() {
+
+}
+
+// 7. When the user presses the enter key in the text input, or clicks the button, set the value of the testString variable (see below) to the value of the input.
+
+
+var testString = "";
+reversal(testString);
+alphabits(testString);
+palindrome(testString);
